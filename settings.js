0aa8252768c4ab57a3cae17dac0409995adda5e9
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-07-10 23:24:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,10 +9,10 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
-global.author = 'Whatsapp Bot 2022'
+global.ownerNumber = ['6285851955578','6282373158947','0']
+global.ownerName = 'Faiq X Radhin'
+global.packname = 'Leviathan Corporation'
+global.author = 'Whatsapp Bot 2022 by Leviathan.inc'
 global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
